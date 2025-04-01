ab40102c8c778dac55c2cc6b0f33d9902eae9765
Tomislav Dukez <tomdu3@ymail.com>
2025-04-01 19:45:52 +0100

    add basic server and connect to db

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log('Hello server')
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB (use MongoDB Atlas for cloud)
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Flight API Running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
