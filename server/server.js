7e93d15288c645b8b05370739413f9c33b9dec13
Tomislav Dukez <tomdu3@ymail.com>
2025-04-03 09:33:25 +0100

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB (use MongoDB Atlas for cloud)
+
+// Database connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Test route
 app.get('/', (req, res) => {
