9ba5de8a1c99b1bb747b8c47daaffacdc1ae6fa8
Tomislav Dukez <tomdu3@ymail.com>
2025-04-07 12:19:10 +0100

    fix server error details

--- server/server.js
+++ server/server.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-
 // Database connection
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Test route
-app.get('/', (req, res) => {
-  res.send('Flight API Running');
+app.get("/", (req, res) => {
+  res.send("Flight API Running");
 });
 
 // Routes
-app.use('/api/auth', authRoutes);
-
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
