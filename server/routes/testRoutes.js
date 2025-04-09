2639e67aee440c0b22b7120fe5a22ded726aadce
Tomislav Dukez <tomdu3@ymail.com>
2025-04-09 17:14:30 +0100

    add test routes

---
+++ server/routes/testRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const authenticateToken = require('../middleware/jwt');
+const router = express.Router();
+
+router.get('/', authenticateToken, (req, res) => {
+    res.json({ message: 'Test route' });
+});
+
+module.exports = router;
