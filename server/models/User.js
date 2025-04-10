f9298a086f75af94b25adce3d2577a2c984e2d97
Tomislav Dukez <tomdu3@ymail.com>
2025-04-10 12:27:21 +0100

    fix User model hashing passwords

--- server/models/User.js
+++ server/models/User.js
@@ -13,10 +13,28 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
+// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Method to update password
+userSchema.methods.updatePassword = async function(newPassword) {
+  this.password = newPassword;
+  await this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);
