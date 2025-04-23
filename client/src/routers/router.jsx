663111f2f1f81c09dfa6d1a0b1abe5d7a4f23a31
Tomislav Dukez <tomdu3@ymail.com>
2025-04-23 16:56:54 +0100

    add  About page

--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import { Home } from '../pages/Home';
+import { About } from '../pages/About';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
+      {
+        path: '/about',
+        element: <About />,
+      }
     ],
   },
 ]);
