456fae041b483e99c372ecdcccbe7830a193a10a
Tomislav Dukez <tomdu3@ymail.com>
2025-04-23 16:54:43 +0100

    add Outlet component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline underline-offset-4 text-red-500">Flight Booking</h1>
+      <Outlet />
     </>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import {Navbar} from '../components/Navbar';
+
 export const Home = () => {
   return (
     <>
+        <Navbar />
       <h1>Home</h1>
     </>
   )
