833906871faa45f84df105b03c9322d3cac3d52f
Tomislav Dukez <tomdu3@ymail.com>
2025-04-23 15:12:21 +0100

    add redux tailwind and router dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline underline-offset-4 text-red-500">Flight Booking</h1>
     </>
   )
 }
