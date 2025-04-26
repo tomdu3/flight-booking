c871985bc6a91ac30ea0229ae552ad5ba8f4535c
Tomislav Dukez <tomdu3@ymail.com>
2025-04-26 16:42:53 +0100

    add FlightSearch component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {Navbar} from '../components/Navbar';
 import desktopBackground from '../assets/img/bg-beach.webp';
 import mobileBackground from '../assets/img/bg-beach-mobile.webp';
+import { FlightSearch } from '../components/FlightSearch';
 
 export const Home = () => {
   return (
     <>
-      <div className="background-container h-screen w-screen">
+      <div className="background-container h-screen w-screen" aria-label='page container and background'>
         <Navbar />
+        <FlightSearch />
       </div>
+
     </>
   )
 }
