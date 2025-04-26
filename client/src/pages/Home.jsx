00615a6060e3cf1be0e222c377d8e80dc4810871
Tomislav Dukez <tomdu3@ymail.com>
2025-04-26 17:14:41 +0100

    update Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,10 @@ import {Navbar} from '../components/Navbar';
 import desktopBackground from '../assets/img/bg-beach.webp';
 import mobileBackground from '../assets/img/bg-beach-mobile.webp';
 import { FlightSearch } from '../components/FlightSearch';
+import { FlightOffers } from '../components/FlightOffers';
+import { TopDestinations } from '../components/TopDestinations';
+import { Contact } from '../components/Contact';
+import { Footer } from '../components/Footer';
 
 export const Home = () => {
   return (
@@ -10,6 +14,10 @@ export const Home = () => {
       <div className="background-container h-screen w-screen" aria-label='page container and background'>
         <Navbar />
         <FlightSearch />
+        <FlightOffers />
+        <TopDestinations />
+        <Contact />
+        <Footer />
       </div>
 
     </>
