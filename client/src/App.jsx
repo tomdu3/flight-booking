c818b035010000fd8fbc6b91ee1a54411bf44097
Tomislav Dukez <tomdu3@ymail.com>
2025-04-26 14:20:09 +0100

    update Home and App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline underline-offset-4 text-red-500">Flight Booking</h1>
       <Outlet />
     </>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import {Navbar} from '../components/Navbar';
+import desktopBackground from '../assets/img/bg-beach.webp';
+import mobileBackground from '../assets/img/bg-beach-mobile.webp';
 
 export const Home = () => {
   return (
     <>
+      <div className="background-container h-screen w-screen">
         <Navbar />
-      <h1>Home</h1>
+      </div>
     </>
   )
 }
