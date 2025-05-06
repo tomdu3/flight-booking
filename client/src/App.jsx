1bb5340687bcf9f29eaac45f58b03fad4dd481f6
Tomislav Dukez <tomdu3@ymail.com>
2025-05-06 10:34:48 +0100

    add toasts to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 function App() {
   return (
     <>
+      <ToastContainer position="top-right" autoClose={3000} />
       <div className="background-container" aria-hidden="true"></div>
       <div className="min-h-screen">
         <Outlet />
